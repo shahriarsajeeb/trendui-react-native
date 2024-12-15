25408fae01ec82815a8be928318ff96d14adee73
tacheraSasi <tacherasasi@gmail.com>
2024-12-15 03:09:09 +0300

    render the installation md on the docs page

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,10 +1,12 @@
 import { Metadata } from "next";
 import { Introduction } from "@/components/docs/Introduction";
+import InstallationPage from "@/components/docs/Installation";
 
 export default function DocsPage() {
   return (
     <div className="space-y-10">
       <Introduction />
+      <InstallationPage/>
     </div>
   );
 }
